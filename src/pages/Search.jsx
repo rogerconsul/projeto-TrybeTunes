79116e9efe1cd672a0cc6e2e019931329b489c22
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-08 15:08:12 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,50 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      habilitado: false,
+    };
+
+    this.verificaInput = this.verificaInput.bind(this);
+  }
+
+  verificaInput(input) {
+    if (input.target.value.length > 1) {
+      this.setState({
+        habilitado: true,
+      });
+    } else {
+      this.setState({
+        habilitado: false,
+      });
+    }
+  }
+
   render() {
+    const { habilitado } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            name="searchInput"
+            id="SearchInput"
+            placeholder="Nome do Artista"
+            data-testid="search-artist-input"
+            onChange={ this.verificaInput }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ !habilitado }
+          >
+            Pesquisar
+          </button>
+        </form>
         <p>Search</p>
       </div>
     );
