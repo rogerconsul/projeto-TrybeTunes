fbe48f63d0d8a856b1debb838466fb4b64804672
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-08 17:20:03 -0300

    Consegui resolver o 6 apos criar um componente separado

---
+++ src/components/CardAlbum.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const {
+      album: { artistName,
+        artworkUrl100,
+        collectionName,
+        collectionId,
+      },
+    } = this.props;
+    return (
+      <Link
+        to={ `/album/${collectionId}` }
+        data-testid={ `link-to-album-${collectionId}` }
+      >
+        <div>
+          <img src={ artworkUrl100 } alt={ artistName } />
+          <h3>{ collectionName }</h3>
+          <h4>{ artistName }</h4>
+        </div>
+      </Link>
+    );
+  }
+}
+
+Card.propTypes = {
+  album: PropTypes.shape({
+    artistName: PropTypes.string.isRequired,
+    artworkUrl100: PropTypes.string.isRequired,
+    collectionName: PropTypes.string.isRequired,
+    collectionId: PropTypes.string.isRequired,
+  }).isRequired,
+};
+export default Card;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Card from '../components/CardAlbum';
 
 class Search extends React.Component {
   constructor() {
@@ -7,13 +10,36 @@ class Search extends React.Component {
 
     this.state = {
       habilitado: false,
+      carregando: false,
+      valorInput: '',
+      albuns: [],
+      procurou: false,
+      pesquisado: '',
     };
 
     this.verificaInput = this.verificaInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  verificaInput(input) {
-    if (input.target.value.length > 1) {
+  handleClick() {
+    const { valorInput } = this.state;
+    this.setState({
+      carregando: true,
+      pesquisado: valorInput,
+    }, async () => {
+      const data = await searchAlbumsAPI(valorInput);
+      this.setState(() => ({
+        carregando: false,
+        valorInput: '',
+        albuns: [...data],
+        procurou: true,
+      }));
+      console.log(data);
+    });
+  }
+
+  verificaInput({ target }) {
+    if (target.value.length > 1) {
       this.setState({
         habilitado: true,
       });
@@ -22,32 +48,58 @@ class Search extends React.Component {
         habilitado: false,
       });
     }
+    this.setState({
+      valorInput: target.value,
+    });
   }
 
   render() {
-    const { habilitado } = this.state;
+    const {
+      habilitado,
+      carregando,
+      procurou,
+      albuns,
+      pesquisado,
+    } = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-        <form>
-          <input
-            type="text"
-            name="searchInput"
-            id="SearchInput"
-            placeholder="Nome do Artista"
-            data-testid="search-artist-input"
-            onChange={ this.verificaInput }
-          />
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            disabled={ !habilitado }
-          >
-            Pesquisar
-          </button>
-        </form>
-        <p>Search</p>
-      </div>
+      carregando ? <Loading /> : (
+        <div data-testid="page-search">
+          <Header />
+          <form>
+            <input
+              type="text"
+              name="searchInput"
+              id="SearchInput"
+              placeholder="Nome do Artista"
+              data-testid="search-artist-input"
+              onChange={ this.verificaInput }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ !habilitado }
+              onClick={ this.handleClick }
+            >
+              Pesquisar
+            </button>
+          </form>
+          {
+            // Unico jeito que consegui foi após criar um componente separado.
+            // Precisei de tanta ajuda das salas de estudo que quase desisti.
+            // https://dev.to/duomly/how-to-write-if-statement-in-react-js-3j9e
+            // pra conseguir fazer os ternários com duas condicionais
+          }
+          { procurou && (
+            <h3>{`Resultado de álbuns de: ${pesquisado}`}</h3>
+          )}
+          { albuns.length === 0 ? (
+            <h3>Nenhum álbum foi encontrado</h3>
+          ) : (
+            albuns.map((album) => <Card key={ album.collectionId } album={ album } />)
+          )}
+
+        </div>
+      )
     );
   }
 }
